e9a21c1fb905ce2ba4e1fe6262f950dd417f643c
Brian McFee <brian.mcfee@nyu.edu>
2017-02-05 12:38:46 -0500

    rewrote parser to use pandas, 2fa-enabled password caching. it works

--- send-invitations.py
+++ send-invitations.py
@@ -5,7 +5,7 @@ import yaml
 
 import jinja2
 import mimetypes
-import numpy as np
+import pandas as pd
 import os
 import smtplib
 import sys
@@ -20,61 +20,67 @@ from email.mime.image import MIMEImage
 from email.mime.application import MIMEApplication
 
 if len(sys.argv) < 3:
-    print('Usage: ./send-invitations.py invitees.csv invitation.txt.in')
+    print('Usage: ./send-invitations.py invitees.tsv invitation.txt.in')
 
 def get_comments(arr):
-  if len(arr['comments']) > 0:
-    return arr['comments']
-  else :
-    return ''
+    if len(arr['comments']) > 0:
+        return str(arr['comments'])
+    else:
+        return ''
 
 def get_recipients(arr):
-  l=[arr['email1'],arr['email2'],arr['email3']]
-  r=[]
-  for item in l:
-    if len(item) > 0:
-      r.append(item)
-  comma=", "
-  return comma.join(r)
+    l=[arr['email1'], arr['email2'], arr['email3']]
+    r=[]
+    for item in l:
+        if len(item) > 0:
+            r.append(str(item))
+    comma=", "
+    return comma.join(r)
+
 
 with open(sys.argv[2], 'r') as fp:
     email_template = fp.read()
 template = jinja2.Template(email_template)
 
-invitees = np.loadtxt(sys.argv[1],
-        delimiter='\t',
-        dtype={'names': ('invitee','email1','email2','email3','comments'),
-            'formats': ('S128', 'S128', 'S128', 'S128', 'S256')})
+invitees = pd.read_csv(sys.argv[1],
+                       delimiter='\t',
+                       header=None,
+                       names=['invitee', 'email1', 'email2', 'email3', 'comments'],
+                       dtype=str,
+                       na_filter=False)
 
 # Load the configuration
-
 with open('config.yaml', 'r') as fp:
     config = yaml.load(fp)
 
+# Load the pdf
 filename="resources/invitation.pdf"
-password = getpass.getpass('password:')
+with open(filename, 'rb') as fp:
+    attachment = fp.read()
+
 server = smtplib.SMTP(config['smtpserver'])
 server.starttls()
-server.login(config['username'], password)
+server.login(config['username'], config['apppass'])
 
 
-for family in tqdm(invitees):
+for family in tqdm(invitees.iterrows()):
+    family = dict(family[1])
     email_body = template.render(guests=family['invitee'],
-                                 comments = get_comments(family))
+                                 comments=get_comments(family))
     msg = MIMEMultipart('alternative')
     msg['Subject'] = 'Invitation to the Wedding of {}'.format(config['wedders'])
     msg['From'] = config['from']
-    msg['To'] = get_recipients(family)
+    msg['To'] = str(get_recipients(family))
     msg['Cc'] = config['cc']
     msg['Date'] = email.utils.formatdate()
     msg.attach(MIMEText(email_body, 'plain'))
-    part=MIMEApplication(open(filename).read()) 
+    part=MIMEApplication(attachment)
     part.add_header('Content-Disposition',
-            'attachment; filename="%s"' % os.path.basename(filename))
+                    'attachment; filename="%s"' % os.path.basename(filename))
     msg.attach(part)
     from_address = config['from']
     to_address = []
-    to_address.extend([em.strip() for em in get_recipients(family).split(', ')])
+    to_address.extend([str(em.strip()) for em in get_recipients(family).split(', ')])
 
-    print(email_body)
-    #server.sendmail(from_address, to_address, msg.as_string())
+    #print(email_body)
+    server.sendmail(from_address, to_address, msg.as_string())
