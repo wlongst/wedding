d5a3c12cab36aa8557d1abc0f581f9550ea2286d
Brian McFee <brian.mcfee@nyu.edu>
2017-02-27 19:55:49 -0500

    cleaning up before rewriting

--- send-invitations.py
+++ send-invitations.py
@@ -4,29 +4,27 @@ from tqdm import tqdm
 import yaml
 
 import jinja2
-import mimetypes
 import pandas as pd
 import os
 import smtplib
 import sys
-import getpass
 import email
 import email.utils
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-# Here are the email package modules we'll need
-from email.mime.image import MIMEImage
 from email.mime.application import MIMEApplication
+
 
 if len(sys.argv) < 3:
     print('Usage: ./send-invitations.py invitees.tsv invitation.txt.in')
+
 
 def get_comments(arr):
     if len(arr['comments']) > 0:
         return str(arr['comments'])
     else:
         return ''
+
 
 def get_recipients(arr):
     l=[arr['email1'], arr['email2'], arr['email3']]
@@ -40,6 +38,7 @@ def get_recipients(arr):
 
 with open(sys.argv[2], 'r') as fp:
     email_template = fp.read()
+
 template = jinja2.Template(email_template)
 
 invitees = pd.read_csv(sys.argv[1],
@@ -82,5 +81,5 @@ for family in tqdm(invitees.iterrows()):
     to_address = []
     to_address.extend([str(em.strip()) for em in get_recipients(family).split(', ')])
 
-    #print(email_body)
-    server.sendmail(from_address, to_address, msg.as_string())
+    print(email_body)
+    #server.sendmail(from_address, to_address, msg.as_string())
